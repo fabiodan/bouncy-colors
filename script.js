2faa073ee7d468a0a315086a9bacabba635f04f8
Fabio Dan <dan.fabio@gmail.com>
2012-03-14 14:26:28 -0300

    A single ball moving through the X and Y axes.

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
-// Shim layer with setTimeout fallback.
-window.requestAnimFrame = (function() {
-    "use strict";
+    // Shim layer with setTimeout fallback.
+    window.requestAnimFrame = (function() {
+        "use strict";
 
-    return  window.requestAnimationFrame       || 
-            window.webkitRequestAnimationFrame || 
-            window.mozRequestAnimationFrame    || 
-            window.oRequestAnimationFrame      || 
-            window.msRequestAnimationFrame     || 
-            function(callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-})();
+        return  window.requestAnimationFrame       || 
+                window.webkitRequestAnimationFrame || 
+                window.mozRequestAnimationFrame    || 
+                window.oRequestAnimationFrame      || 
+                window.msRequestAnimationFrame     || 
+                function(callback) {
+                    window.setTimeout(callback, 1000 / 60);
+                };
+    })();
 
-// Singleton
-var bouncyColors = (function() {
-    "use strict";
+    // Singleton
+    var bouncyColors = (function() {
+        "use strict";
 
-    var game = document.querySelector("#bouncy-colors");
-    var canvas = game.querySelector("canvas");
-    var ctx = canvas.getContext("2d");
-    var ball = {
-        x : 50,
-        y : 50,
-        radius : 10,
-        color : "#0000FF",
-        speed : 5
-    };
+        var game = document.querySelector("#bouncy-colors");
+        var canvas = game.querySelector("canvas");
+        var ctx = canvas.getContext("2d");
+        var ball = {
+            x : 50,
+            y : 50,
+            radius : 10,
+            color : "#0000FF",
+            direction : 35, // Degrees.
+            speed : 5
+        };
 
-    // Draw the screen relying on objects data.
-    function draw() {
-        ctx.beginPath();
+        // Draw the screen relying on objects data.
+        function draw() {
+            ctx.beginPath();
 
-        // Cleaning screen.
-        ctx.clearRect(0, 0, 500, 500);
+            // Cleaning screen.
+            ctx.clearRect(0, 0, 500, 500);
 
-        // Drawing ball.
-        ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-        ctx.fillStyle = ball.color;
-        ctx.fill();
-    }
-
-    // Update game objects data.
-    function update() {
-        ball.x = ball.x + 5;
-    }
-
-    function gameCycle() {
-        window.requestAnimFrame(gameCycle);
-        draw();
-        update();
-    }
-
-    return {
-        init : function() {
-            gameCycle();
+            // Drawing ball.
+            ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+            ctx.fillStyle = ball.color;
+            ctx.fill();
         }
-    };
-})();
 
-bouncyColors.init();
+        // Update game objects data.
+        function update() {
+
+            // JavaScript trig functions expect or return angles in radians.
+            var radians = ball.direction * Math.PI / 180;
+
+            // Using trig functions to find the x and y values.
+            ball.x += ball.speed * Math.cos(radians);
+            ball.y += ball.speed * Math.sin(radians);
+        }
+
+        function gameCycle() {
+            window.requestAnimFrame(gameCycle);
+            draw();
+            update();
+        }
+
+        return {
+            init : function() {
+                gameCycle();
+            }
+        };
+    })();
+
+    bouncyColors.init();
